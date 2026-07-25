aed2499cd4ca6688ff7e7e0704cec1ce49f6e427
kavinraja <kavinraja4613@gmail.com>
2026-07-25 20:05:13 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,3 +1,9 @@
 import random
 import string
-length=int(input ("Enter the length of the password:"))
+length=int(input("Enter the length of the password: "))
+Random=random.choice(string.ascii_lowercase)
+Random1=random.choice(string.ascii_uppercase)
+password=""
+for i in range (length):
+    password+=random.choice(string.ascii_lowercase)+random.choice(string.ascii_uppercase)
+print(password)
